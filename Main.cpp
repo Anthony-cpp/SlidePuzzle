f5565f4c740d07314817e869cf544c5301823453
Anthony-cpp <54949075+Anthony-cpp@users.noreply.github.com>
2022-08-16 14:44:09 +0900

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -72,33 +72,6 @@ void reset(int l,int m) {
 
 }
 
-void g_option(){
-
-	const Font font{ 30 };
-
-	String text;
-
-	constexpr Rect area{ 50, 50, 700, 300 };
-
-	while (System::Update())
-	{
-
-	if (SimpleGUI::Button(U"background", Vec2{ 500, 500 }, 100))
-	{
-			flag2 = 1;
-			return;
-	}
-
-
-	if (SimpleGUI::Button(U"back", Vec2{ 20, 500 }, 100))
-	{
-		flag2 = 1;
-		return;
-	}
-	}
-}
-
-
 void game(int l,int m){
 
 	// 背景の色を設定 | Set background color
@@ -318,6 +291,160 @@ void game(int l,int m){
 
 }
 
+void g_option() {
+
+	const Font font{ 30 };
+
+	String text;
+
+
+	while (System::Update())
+	{
+		font(text).drawAt(Vec2(400, 150), ColorF{ Palette::Black });
+
+		if (SimpleGUI::Button(U"background", Vec2{ 500, 100 }, 150))
+		{
+			flag2 = 8;
+			return;
+		}
+
+		if (SimpleGUI::Button(U"盤面", Vec2{ 500, 200 }, 150))
+		{
+			flag2 = 9;
+			return;
+		}
+
+		if (SimpleGUI::Button(U"パネル", Vec2{ 500, 300 }, 150))
+		{
+			flag2 = 10;
+			return;
+		}
+
+		if (SimpleGUI::Button(U"リセット", Vec2{ 500, 400 }, 150))
+		{
+			flag2 = 11;
+			return;
+		}
+
+
+		if (SimpleGUI::Button(U"back", Vec2{ 20, 500 }, 100))
+		{
+			flag2 = 1;
+			return;
+		}
+	}
+}
+
+void reset_c() {
+
+	r1 = 0.8, g1 = 0.9, b1 = 1.0;
+	r2 = 0.6, g2 = 0.27, b2 = 0.0;
+	r3 = 1.0, g3 = 0.7, b3 = 0.02;
+
+	flag2 = 1;
+
+}
+
+void ch_color(double r, double g, double b,int type) {
+
+	const Font font{ 30 };
+
+	String text;
+
+	double x1 = 60 + 600 * r;
+	double x2 = 60 + 600 * g;
+	double x3 = 60 + 600 * b;
+
+	bool grab = 0;
+
+	while (System::Update())
+	{
+
+		int ra = r * 255;
+		int ga = g * 255;
+		int ba = b * 255;
+
+		text = U"{} {} {}"_fmt(ra,ga,ba);
+
+		font(text).drawAt(Vec2(600, 500), ColorF{ Palette::Black });
+
+		Scene::SetBackground(ColorF{ r, g, b });
+
+
+		double mx = Cursor::Pos().x;
+		double my = Cursor::Pos().y;
+
+		Rect{ 60, 80, 600, 20 }.draw(Palette::Black);
+
+		Rect{ 60, 200, 600, 20 }.draw(Palette::Black);
+
+		Rect{ 60, 320, 600, 20 }.draw(Palette::Black);
+
+		Circle{ x1, 90, 40 }.draw(Palette::Black);
+
+		Circle{ x2, 210, 40 }.draw(Palette::Black);
+
+		Circle{ x3, 330, 40 }.draw(Palette::Black);
+
+		Circle{ x1, 90, 35 }.draw();
+
+		Circle{ x2, 210, 35 }.draw();
+
+		Circle{ x3, 330, 35 }.draw();
+
+
+		if (MouseL.pressed())  grab = 1;
+
+		else grab = 0;
+
+
+		if (grab) {
+
+			if ((mx - x1) * (mx - x1) + (my - 90) * (my - 90) <= 1600) {
+				if(mx <= 660 && mx >= 60) x1 = mx;
+				r = (x1 - 60) / 600;
+			}
+
+
+			if ((mx - x2) * (mx - x2) + (my - 210) * (my - 210) <= 1600) {
+				if (mx <= 660 && mx >= 60) x2 = mx;
+				g = (x2 - 60) / 600;
+			}
+
+
+			if ((mx - x3) * (mx - x3) + (my - 330) * (my - 330) <= 1600) {
+				if (mx <= 660 && mx >= 60) x3 = mx;
+				b = (x3 - 60) / 600;
+			}
+
+		}
+
+		if (type == 1) {
+			r1 = r;
+			g1 = g;
+			b1 = b;
+		}
+
+		if (type == 2) {
+			r2 = r;
+			g2 = g;
+			b2 = b;
+		}
+
+		if (type == 3) {
+			r3 = r;
+			g3 = g;
+			b3 = b;
+		}
+
+		if (SimpleGUI::Button(U"back", Vec2{ 20, 500 }, 100))
+		{
+			flag2 = 1;
+			return;
+		}
+
+	}
+}
 
 void Main(){
 
@@ -337,6 +464,10 @@ void Main(){
 		if (flag2 == 5) game(5,5);
 		if (flag2 == 6) game(6, 6);
 		if (flag2 == 7) g_option();
+		if (flag2 == 8) ch_color(r1,g1,b1,1);
+		if (flag2 == 9) ch_color(r2, g2, b2,2);
+		if (flag2 == 10) ch_color(r3, g3, b3,3);
+		if (flag2 == 11) reset_c();
 
 		if (flag2 == 0) {
 			text = U"Slide Puzzle";
